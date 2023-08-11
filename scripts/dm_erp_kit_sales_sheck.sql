4d990482653ef87448deacfa220d05ec270afbba
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-08-11 17:04:32 +0300

    dobavil proverki i vitrinu

--- scripts/dm_erp_kit_sales_sheck.sql
+++ scripts/dm_erp_kit_sales_sheck.sql
@@ -4,4 +4,4 @@ SELECT
     '{{execution_date.date()}}',
     SUM()={{task_instance.xcom_pull(key='stage_erp_kit_sales', task_ids='Загрузка_данных_в_stage_слой.stage_erp_kit_sales')}}
 FROM sttgaz.{{params.dm}}
-WHERE "Месяц" = {{execution_date.date().replace(day=1)}};
+WHERE "Месяц" = '{{execution_date.date().replace(day=1)}}';
