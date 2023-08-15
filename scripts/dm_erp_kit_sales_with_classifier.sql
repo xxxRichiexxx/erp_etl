d6624862463f21443cb2d6cab718c4c61d8b50e4
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-08-15 18:33:25 +0300

    dm change

---
+++ scripts/dm_erp_kit_sales_with_classifier.sql
@@ -0,0 +1,17 @@
+SELECT
+	Месяц,
+	COALESCE(ks.Дивизион, n.Division)  		AS Дивизион,
+	Контрагент,
+	"Направление реализации с учетом УКП",
+	Реализовано,
+	Name 									AS Товар,
+	Code65 									AS ТоварКод65,
+	Manufacture								AS Производитель,
+	property_value_name_1					AS "Классификатор подробно по дивизионам 22"
+FROM sttgaz.dm_erp_kit_sales_v ks
+LEFT JOIN sttgaz.stage_isc_nomenclature_guide n
+ON ks."Чертежный номер комплекта" = n.ManufactureModel
+	AND n.ManufactureModel <> ''
+LEFT JOIN sttgaz.dm_isc_classifier_v c 
+	ON n.Code65  = c.product_name 
+	AND c.property_name = 'Подробный по дивизионам (с 2022 г)'
