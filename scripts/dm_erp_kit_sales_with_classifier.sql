3418796f710a81bc3faab43d8803584816eef6f1
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-08-16 09:29:54 +0300

    dds change

--- scripts/dm_erp_kit_sales_with_classifier.sql
+++ scripts/dm_erp_kit_sales_with_classifier.sql
@@ -10,7 +10,7 @@ SELECT
 	property_value_name_1					AS "Классификатор подробно по дивизионам 22"
 FROM sttgaz.dm_erp_kit_sales_v ks
 LEFT JOIN sttgaz.stage_isc_nomenclature_guide n
-ON ks."Чертежный номер комплекта" = n.ManufactureModel
+ON ks."Чертежный номер комплекта" = REGEXP_REPLACE(n.ManufactureModel, '^А', 'A') 
 	AND n.ManufactureModel <> ''
 LEFT JOIN sttgaz.dm_isc_classifier_v c 
 	ON n.Code65  = c.product_name 
